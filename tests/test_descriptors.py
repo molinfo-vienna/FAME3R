56f1391ab9a576bfdacf918bff479eeb4a76b4c4
Leo Gaskin <leo.gaskin@le0.gs>
2025-02-07 20:15:49 +0100

    Quickly fix formatting in new test

--- tests/test_descriptors.py
+++ tests/test_descriptors.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pytest
 from CDPL import Chem
+
 from src.compute_descriptors import FAMEDescriptors
 
 inputs_dir: Path = Path(__file__).parent.joinpath("inputs")
@@ -24,10 +25,10 @@ def test_descriptor_dimensions(mol):
     labels, desc = FAMEDescriptors(radius=2)._process_molecule(mol, has_soms=False)
     non_hydrogen_atoms = [atom for atom in mol.atoms if Chem.getSybylType(atom) != 24]
 
-    assert len(desc) == len(non_hydrogen_atoms), (
-        "descriptors are generated for each non-hydrogen atom"
-    )
+    assert len(desc) == len(
+        non_hydrogen_atoms
+    ), "descriptors are generated for each non-hydrogen atom"
 
-    assert all([len(atom_desc) == len(labels) for _, atom_desc in desc.values()]), (
-        "each atom has the correct amount of descriptors"
-    )
+    assert all(
+        [len(atom_desc) == len(labels) for _, atom_desc in desc.values()]
+    ), "each atom has the correct amount of descriptors"
