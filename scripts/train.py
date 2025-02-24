ba8a05fb64ef99d241ad9d83782894180aa9aecf
Leo Gaskin <leo.gaskin@le0.gs>
2025-02-24 13:54:56 +0100

    Include script entry points in package
    This ensures that people who install the package can run the scripts as
    normal CLI commands without thinking about Python.

--- scripts/train.py
+++ scripts/train.py
@@ -53,7 +53,7 @@ def parse_arguments() -> argparse.Namespace:
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+def main():
     start_time = datetime.now()
 
     args = parse_arguments()
@@ -93,3 +93,7 @@ if __name__ == "__main__":
     print("Finished in:", datetime.now() - start_time)
 
     sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
