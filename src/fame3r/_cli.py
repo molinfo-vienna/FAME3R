a20157e9637dded5c2f4da7f384d94cd48b3d643
Leo Gaskin <leo.gaskin@le0.gs>
2025-08-19 13:08:55 +0200

    Rename "--compute-fame-score" argument to "--fame-score"

--- src/fame3r/_cli.py
+++ src/fame3r/_cli.py
@@ -257,7 +257,7 @@ def infer(
     compute_fame_score: Annotated[
         bool,
         typer.Option(
-            "--compute-fame-score", help="Also compute FAME score for applicability."
+            "--fame-score", help="Also compute FAME score for applicability."
         ),
     ] = False,
 ):
